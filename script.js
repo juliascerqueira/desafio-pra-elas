9532a9ebc4ae0e078811d6b329a4b1175fc89950
Jessica Kopps <jessicakopps@gmail.com>
2021-10-06 06:08:07 -0300

    Paginas Inicial e Post

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+// ------------------- Botões curso -------------------------->
+
+document.getElementById("investimento-button").onclick = function () {
+  location.href = "./pages/cursos/investimento.html";
+};
+
+document.getElementById("independencia-button").onclick = function () {
+  location.href = "./pages/cursos/investimento.html";
+};
+
+document.getElementById("tradition-button").onclick = function () {
+  location.href = "./pages/cursos/investimento.html";
+};
+
+// ------------------- Botão blog -------------------------->
+
+document.getElementById("blog-button").onclick = function () {
+  location.href = "./pages/blog/blog.html";
+};
